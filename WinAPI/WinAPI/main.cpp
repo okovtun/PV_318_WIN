dab9eb4cf1b63e76fee67875600c1d8dbceb041f
User <User@DESKTOP-0TCDSR6>
2024-09-27 22:11:14 +0300

    DialogBox

---
+++ WinAPI/WinAPI/main.cpp
@@ -0,0 +1,79 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+//Процедура окна:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//hInstance - экземпляр запущенного *.exe-файла нашей программы
+	//hPrevInst - НЕ используется
+	//LPSTR - Long Pointer To String
+	//lpCmdLine - CommandLine (командная строка с параметрами запуска приложения)
+	//nCmdShow  - режим отображения окна (развернуто на весь экран, cвернуто на панель задач и т.д.)
+	//Префиксы: n..., lp... это Венгерская нотация
+	//			n - Number
+	//			lp - Long Pointer
+	//			h - HINSTANCE
+
+	/*MessageBox
+	(
+		NULL,
+		"Hello Windows!\nThis is MessageBox()",
+		"Window title",
+		MB_CANCELTRYCONTINUE | MB_HELP | MB_DEFBUTTON3 |
+		MB_ICONINFORMATION |
+		MB_SERVICE_NOTIFICATION |
+		MB_RIGHT
+	);*/
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
+
+	return 0;
+}
+
+//Процедура окна:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	//hwnd - Handler to Window. Обработчик или дескриптор окна - это число, при помощи которого можно обратиться к окну.
+	//uMsg - Message. Сообщение, которое отправляется окну.
+	//wParam, lParam - это параметры сообщения, у каждого сообщения свой набор параметров.
+	//LOWORD - Младшее слово;
+	//HIWORD - Старшее слово;
+	//DWORD  - Double Word;
+
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	//Это сообшение отправляется 1 раз при инициализации окна
+		break;
+	case WM_COMMAND:	//Обрабатывает нажатие кнопок и другие действия пользователя
+		//ResourceID
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_COPY:
+		{
+			//1) Создаем буфер:
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};	//sz_ - String Zero (NULL Terminated Line - C-string)
+
+			//2) Получаем обработчики текстовых полей:
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+
+			//3) Считываем содержимое поля 'Login' в буфер:
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			//4) Записываем полученный текст в текстовое поле 'Password':
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL:	EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:		//Отправляется при нажатии кнопки 'Закрыть' X
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
