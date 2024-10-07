4df67841f702198b59296e9ee391c235b5006c95
User <User@DESKTOP-0TCDSR6>
2024-10-07 22:20:42 +0300

    Main Window day 1

---
+++ WinAPI/MainWindow/main.cpp
@@ -0,0 +1,89 @@
+﻿#include<Windows.h>
+
+//MDI - Multi Document Interface
+
+CONST CHAR g_sz_WINDOW_CLASS[] = "Main Window";	//Имя класса окна.
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//1) Регистрация класса окна:
+	//		1.1. Создать и проинициализировать структуру WNDCLASSEX:
+	WNDCLASSEX wClass;
+	ZeroMemory(&wClass, sizeof(wClass));
+
+	wClass.style = NULL;
+	wClass.cbSize = sizeof(wClass);	//cb - Count Bytes
+	wClass.cbClsExtra = 0;	//Class Extra Bytes
+	wClass.cbWndExtra = 0;	//Window Extra Bytes
+
+	//Appearance:
+	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);	//Small Icon
+	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	//
+	wClass.hInstance = hInstance;
+	wClass.lpfnWndProc = (WNDPROC)WndProc;
+	wClass.lpszClassName = g_sz_WINDOW_CLASS;
+	wClass.lpszMenuName = NULL;
+
+	//		1.2. Вызвать функцию RegisterClassEx(...):
+	if (RegisterClassEx(&wClass) == NULL)
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	//2) Создание окна:
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,	//Window ExStyle
+		g_sz_WINDOW_CLASS,	//Window Class Name
+		g_sz_WINDOW_CLASS,	//Window Title
+		WS_OVERLAPPEDWINDOW,//Window Style
+		CW_USEDEFAULT, CW_USEDEFAULT,	//Coordinates
+		CW_USEDEFAULT, CW_USEDEFAULT,	//Window size
+		NULL,	//Parent
+		NULL,	//hMenu: Для главного окна это ResourceID нлавного меню,
+				//для дочернего окна (кнопки и друние элементы) - 
+				//это ResourceID соответствующего элемента (кнопки, такстового пол, и т.д.)
+				//По этому RousourceID мы находим дочерний элемент окна при помощи функции GetDlgItem()
+		hInstance,
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);	//Задает режим отображения окна (развернуто на весь экран, свернуто в окно, свернуто в панель задач)
+	UpdateWindow(hwnd);	//Прорисовывает окно
+
+	//3) Запуск цикла сообщений:
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.message;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+	case WM_CLOSE:DestroyWindow(hwnd); break;
+	default:	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return FALSE;
+}
