0cc699b47c79abce356b856ee43472fbc8152215
User <User@DESKTOP-0TCDSR6>
2024-10-11 21:21:24 +0300

    Buttons DONE

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -4,13 +4,20 @@
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc";
 
 CONST INT g_BUTTON_SIZE = 50;
-CONST INT g_INTERVAL = 5;
+CONST INT g_INTERVAL = 2;
+CONST INT g_BUTTON_DOUBLE_SIZE = g_BUTTON_SIZE * 2 + g_INTERVAL;
 CONST INT g_START_X = 10;
 CONST INT g_START_Y = 10;
-CONST INT g_DISPLAY_WIDTH = 300;
+CONST INT g_DISPLAY_WIDTH = g_BUTTON_SIZE * 5 + g_INTERVAL * 4;
 CONST INT g_DISPLAY_HEIGHT = 25;
 CONST INT g_BUTTON_START_X = g_START_X;
 CONST INT g_BUTTON_START_Y = g_START_Y + g_DISPLAY_HEIGHT + g_INTERVAL;
+CONST INT g_OPERATIONS_START_X = g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * 3;
+CONST INT g_OPERATIONS_START_Y = g_BUTTON_START_Y;
+CONST INT g_CONTROL_BUTTONS_START_X = g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * 4;
+CONST INT g_CONTROL_BUTTONS_START_Y = g_BUTTON_START_Y;
+
+CONST CHAR g_OPERATIONS[] = "*/-+";
 
 BOOL CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -47,9 +54,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL,	//ExStyle
 		g_sz_WINDOW_CLASS,	//Class name
 		g_sz_WINDOW_CLASS,	//Window title
-		WS_OVERLAPPEDWINDOW,//Window style
+		WS_OVERLAPPEDWINDOW - WS_THICKFRAME - WS_MAXIMIZEBOX,//Window style
 		CW_USEDEFAULT, CW_USEDEFAULT,	//Position
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Size
+		g_DISPLAY_WIDTH + g_BUTTON_START_X * 2 + 16,
+		g_DISPLAY_HEIGHT + (g_BUTTON_SIZE + g_INTERVAL) * 4 + g_START_Y * 2 + 16 + 23,	//Size
 		NULL,	//Parent
 		NULL,	//Menu
 		hInstance,
@@ -87,27 +95,136 @@ BOOL CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			NULL
 		);
 
+		CHAR sz_digit[2]{};
 		for (int i = 6; i >= 0; i -= 3)
 		{
 			for (int j = 0; j < 3; j++)
 			{
+				sz_digit[0] = i + j + '1';
 				CreateWindowEx
 				(
-					NULL, "Button", "0",
+					NULL, "Button", sz_digit,
 					WS_CHILD | WS_VISIBLE,
-					g_BUTTON_START_X + (g_BUTTON_SIZE + g_INTERVAL) * j, g_BUTTON_START_Y + (g_BUTTON_SIZE+g_INTERVAL) * (i / 3),
+					g_BUTTON_START_X + (g_BUTTON_SIZE + g_INTERVAL) * j,
+					g_BUTTON_START_Y + (g_BUTTON_SIZE + g_INTERVAL) * (2 - i / 3),
 					g_BUTTON_SIZE, g_BUTTON_SIZE,
 					hwnd,
-					(HMENU)(IDC_BUTTON_0 + i + j),
+					(HMENU)(IDC_BUTTON_1 + i + j),
 					GetModuleHandle(NULL),
 					NULL
 				);
 			}
 		}
+		CreateWindowEx
+		(
+			NULL, "Button", "0",
+			WS_CHILD | WS_VISIBLE,
+			g_BUTTON_START_X, g_BUTTON_START_Y + (g_BUTTON_SIZE + g_INTERVAL) * 3,
+			g_BUTTON_DOUBLE_SIZE, g_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_0,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", ".",
+			WS_CHILD | WS_VISIBLE,
+			g_BUTTON_START_X + g_BUTTON_DOUBLE_SIZE + g_INTERVAL,
+			g_BUTTON_START_Y + (g_BUTTON_SIZE + g_INTERVAL) * 3,
+			g_BUTTON_SIZE, g_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_POINT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CHAR sz_operation[2]{};
+		for (int i = 0; i < 4; i++)
+		{
+			sz_operation[0] = g_OPERATIONS[i];
+			CreateWindowEx
+			(
+				NULL, "Button", sz_operation,
+				WS_CHILD | WS_VISIBLE,
+				g_OPERATIONS_START_X, g_OPERATIONS_START_Y + (g_BUTTON_SIZE + g_INTERVAL)*i,
+				g_BUTTON_SIZE, g_BUTTON_SIZE,
+				hwnd,
+				(HMENU)IDC_BUTTON_PLUS + i,
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
+		CreateWindowEx
+		(
+			NULL, "Button", "<-",
+			WS_CHILD | WS_VISIBLE,
+			g_CONTROL_BUTTONS_START_X, g_CONTROL_BUTTONS_START_Y,
+			g_BUTTON_SIZE, g_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_BSP,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", "C",
+			WS_CHILD | WS_VISIBLE,
+			g_CONTROL_BUTTONS_START_X, g_CONTROL_BUTTONS_START_Y + g_BUTTON_SIZE + g_INTERVAL,
+			g_BUTTON_SIZE, g_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLEAR,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", "=",
+			WS_CHILD | WS_VISIBLE,
+			g_CONTROL_BUTTONS_START_X, g_CONTROL_BUTTONS_START_Y + (g_BUTTON_SIZE + g_INTERVAL) * 2,
+			g_BUTTON_SIZE, g_BUTTON_DOUBLE_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_EQUAL,
+			GetModuleHandle(NULL),
+			NULL
+		);
 	}
 	break;
 	case WM_COMMAND:
-		break;
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_display[SIZE]{};
+		CHAR sz_digit[2]{};
+		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		{
+			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + 48;
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (sz_display[0] == '0' && sz_display[1] != '.')sz_display[0] = 0;
+			strcat(sz_display, sz_digit);	//Фкнкция strcat(dst, src) выполняет конкатенацию строк,
+			//а именно, к строке dst (Destination - Получатель) добавляет содержимое строки src (Source - Источник)
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_POINT)
+		{
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (strchr(sz_display, '.'))break;
+			strcat(sz_display, ".");
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_BSP)
+		{
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (strlen(sz_display) == 1)sz_display[0] = '0';
+			else sz_display[strlen(sz_display) - 1] = 0;
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
+		{
+			sz_display[0] = '0';
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+	}
+	break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
