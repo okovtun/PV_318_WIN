fffdd49c0516fe3fa17fc319d44f342130d4477a
User <User@DESKTOP-0TCDSR6>
2024-10-16 22:27:21 +0300

    Clock day 1

--- WindowsForms/Clock/MainForm.Designer.cs
+++ WindowsForms/Clock/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Clock
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -29,9 +29,12 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.labelTime = new System.Windows.Forms.Label();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.cbShowDate = new System.Windows.Forms.CheckBox();
+			this.btnHideControls = new System.Windows.Forms.Button();
+			this.notifyIconSystemTray = new System.Windows.Forms.NotifyIcon(this.components);
 			this.SuspendLayout();
 			// 
 			// labelTime
@@ -43,6 +46,7 @@
 			this.labelTime.Size = new System.Drawing.Size(130, 55);
 			this.labelTime.TabIndex = 0;
 			this.labelTime.Text = "Time";
+			this.labelTime.DoubleClick += new System.EventHandler(this.labelTime_DoubleClick);
 			// 
 			// timer1
 			// 
@@ -61,15 +65,39 @@
 			this.cbShowDate.Text = "Показать дату";
 			this.cbShowDate.UseVisualStyleBackColor = true;
 			// 
-			// Form1
+			// btnHideControls
+			// 
+			this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnHideControls.Location = new System.Drawing.Point(22, 196);
+			this.btnHideControls.Name = "btnHideControls";
+			this.btnHideControls.Size = new System.Drawing.Size(230, 78);
+			this.btnHideControls.TabIndex = 2;
+			this.btnHideControls.Text = "Скрыть элементы управления";
+			this.btnHideControls.UseVisualStyleBackColor = true;
+			this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+			// 
+			// notifyIconSystemTray
+			// 
+			this.notifyIconSystemTray.BalloonTipTitle = "Current time";
+			this.notifyIconSystemTray.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconSystemTray.Icon")));
+			this.notifyIconSystemTray.Text = "notifyIcon1";
+			this.notifyIconSystemTray.Visible = true;
+			this.notifyIconSystemTray.MouseMove += new System.Windows.Forms.MouseEventHandler(this.notifyIconSystemTray_MouseMove);
+			// 
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(357, 450);
+			this.Controls.Add(this.btnHideControls);
 			this.Controls.Add(this.cbShowDate);
 			this.Controls.Add(this.labelTime);
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(372, 39);
+			this.Name = "MainForm";
+			this.Text = "Clock";
+			this.TransparencyKey = System.Drawing.Color.White;
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -80,6 +108,8 @@
 		private System.Windows.Forms.Label labelTime;
 		private System.Windows.Forms.Timer timer1;
 		private System.Windows.Forms.CheckBox cbShowDate;
+		private System.Windows.Forms.Button btnHideControls;
+		private System.Windows.Forms.NotifyIcon notifyIconSystemTray;
 	}
 }
 
---
+++ WindowsForms/Clock/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			this.TransparencyKey = Color.Empty;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+			if (cbShowDate.Checked)
+			{
+				labelTime.Text += $"\n{DateTime.Today.ToString("yyyy.MM.dd")}";
+			}
+			//notifyIconSystemTray.Text = "Curret time " + labelTime.Text;
+		}
+		private void SetVisibility(bool visible)
+		{
+			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+			this.FormBorderStyle = visible ? FormBorderStyle.Sizable : FormBorderStyle.None;
+			this.ShowInTaskbar = visible;
+			cbShowDate.Visible = visible;
+			btnHideControls.Visible = visible;
+			labelTime.BackColor = visible ? Color.Empty : Color.Coral;
+		}
+		private void btnHideControls_Click(object sender, EventArgs e)
+		{
+			SetVisibility(false);
+			notifyIconSystemTray.ShowBalloonTip(3, "Alerts!", "Для того чтобы вернуть как было, нужно ткнуть 2 раза мышей по часам, или по этой иконке", ToolTipIcon.Warning);
+		}
+
+		private void labelTime_DoubleClick(object sender, EventArgs e)
+		{
+			SetVisibility(true);
+		}
+
+		private void notifyIconSystemTray_MouseMove(object sender, MouseEventArgs e)
+		{
+			notifyIconSystemTray.Text = "Curret time:\n" + labelTime.Text;
+		}
+	}
+}
